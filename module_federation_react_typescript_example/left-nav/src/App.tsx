40eee90a956462240101d5fd7779f404a6c6916f
dinesh24murali <dinesh24murali@gmail.com>
2024-08-06 10:27:41 +0530

    add: scss

--- module_federation_react_typescript_example/left-nav/src/App.tsx
+++ module_federation_react_typescript_example/left-nav/src/App.tsx
@@ -1,9 +1,10 @@
 import react from "react";
+import './styles/index.scss';
 
 export default function App() {
   return (
     <div>
-      <h1>Left Navbar 1</h1>
+      <h1 className="h1">Left Navbar</h1>
     </div>
   );
 }
--- module_federation_react_typescript_example/right-nav/src/App.tsx
+++ module_federation_react_typescript_example/right-nav/src/App.tsx
@@ -1,9 +1,10 @@
 import react from "react";
+import './styles/index.scss';
 
 export default function App() {
   return (
     <div>
-      <h1>Right Navbar</h1>
+      <h1 className="h2">Right Navbar</h1>
     </div>
   );
 }
